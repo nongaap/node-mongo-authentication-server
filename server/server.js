d0f958aa771be1a17e20b1a374a731727de53a59
Mike Puangmalai <mike@nongaap.com>
2016-09-30 11:18:36 -0700

    Added node cluster to server.

--- server/server.js
+++ server/server.js
@@ -1,34 +1,57 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var register = require('./routes/register');
-var login = require('./routes/login');
+var cluster = require('cluster');
 
-var port = 3000;
+if (cluster.isMaster) {
 
-var app = express();
+  // Count the machine's CPUs
+  var cpuCount = require('os').cpus().length;
 
-var server = app.listen(port, function() {
-  console.log('listening on port 3000');
-});
+  // Create a worker for each CPU
+  for (var i = 0; i < cpuCount; i += 1) {
+      cluster.fork();
+  }
 
-app.use(bodyParser.urlencoded({ extended: true }));
+  // Listen for dying workers
+  cluster.on('exit', function (worker) {
 
-app.use(cookieParser());
+    console.log('Worker died :(', worker.id);
+    cluster.fork();
 
-app.use(bodyParser.json());
+    });
 
-app.use('/register', register);
+} else {
 
-app.use('/login', login);
+  var express = require('express');
+  var path = require('path');
+  var bodyParser = require('body-parser');
+  var cookieParser = require('cookie-parser');
+  var register = require('./routes/register');
+  var login = require('./routes/login');
 
-app.get('/', function (req, res) {
+  var port = 3000;
+
+  var app = express();
+
+  var server = app.listen(port, function() {
+    console.log('listening on port 3000');
+  });
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use(cookieParser());
+
+  app.use(bodyParser.json());
+
+  app.use('/register', register);
+
+  app.use('/login', login);
+
+  app.get('/', function (req, res) {
 	res.status(200).send('Success');
-});
+  });
 
-app.all('*', function (req, res) {
+  app.all('*', function (req, res) {
 	res.status(404).send('Nothing There');
-});
+  });
 
-module.exports = server;
+  module.exports = server;
+}
